f395bde2e73bbd2961f8565a5077dae1b39b0f68
余艳辉 <913918146@qq.com>
2021-10-26 17:58:15 +0800

    1，修改HookUtil的方法为静态。

--- base/src/main/java/com/ke/mvvm/base/util/HookUtil.kt
+++ base/src/main/java/com/ke/mvvm/base/util/HookUtil.kt
@@ -5,29 +5,31 @@ import java.lang.Exception
 
 class HookUtil {
 
-    /**
-     * 是否被hook了
-     */
-    private fun isHook(context: Context): Boolean {
-        val whiteList = listOf(
-            "com.android",
-            "java.lang",
-            "android",
-            context.packageName
-        )
-        try {
-            throw Exception("gg")
-        } catch (e: Exception) {
-            e.stackTrace.forEach { element ->
+    companion object{
+        /**
+         * 是否被hook了
+         */
+        fun isHook(context: Context): Boolean {
+            val whiteList = listOf(
+                "com.android",
+                "java.lang",
+                "android",
+                context.packageName
+            )
+            try {
+                throw Exception("gg")
+            } catch (e: Exception) {
+                e.stackTrace.forEach { element ->
 
-                if (!whiteList.map {
-                        element.className.startsWith(it)
-                    }.contains(true)) {
-                    return true
+                    if (!whiteList.map {
+                            element.className.startsWith(it)
+                        }.contains(true)) {
+                        return true
+                    }
                 }
             }
-        }
 
-        return false
+            return false
+        }
     }
 }
